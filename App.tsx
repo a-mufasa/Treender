22ed62dace12f4a84ff6f42c5dc689771c4acbc7
jakilgo <90350783+jakilgo@users.noreply.github.com>
2023-04-01 03:18:17 -0500

    created signup/in screen (#9)

--- App.tsx
+++ App.tsx
@@ -8,38 +8,39 @@ import TabBarIcon from "./components/TabBarIcon";
 import "react-native-url-polyfill/auto"
 import { onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "./firebaseConfig";
+import { TextInput, TouchableOpacity, View, StyleSheet, Text } from "react-native";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const signInUser = (email: string, password: string) => {
   createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    const user = userCredential.user;
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    if (errorCode === "auth/email-already-in-use") {
-      signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        return errorCode
-      });
-    } else {
-      return errorCode
-    }
-    // ..
-  });
-  }
+    .then((userCredential) => {
+      const user = userCredential.user;
+      // ----------- call create profile screen here ----------- //
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      if (errorCode === "auth/email-already-in-use") {
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+        });
+      }
+    });
+}
   
   const App = () => {
     const [loggedIn, setLoggedIn] = useState(false);
+    const [email, setEmail] = useState("tmp");
+    const [password, setPassword] = useState("tmp")
+
     onAuthStateChanged(auth, (user) => {
       if (user) {
         setLoggedIn(true);
@@ -139,10 +140,77 @@ const signInUser = (email: string, password: string) => {
       </NavigationContainer>
         )
       } else {
-        const email = "email@email.com"
-        const password = "asdf1234"
-        signInUser(email, password);
+        return (
+          <View style={styles.container}>
+            <Text style={styles.logo}>Treender</Text>
+            <View style={styles.inputView} >
+              <TextInput  
+                style={styles.inputText}
+                placeholder="Email..." 
+                placeholderTextColor="#003f5c"
+                onChangeText={text => setEmail(text)}/>
+            </View>
+            <View style={styles.inputView} >
+              <TextInput  
+                secureTextEntry
+                style={styles.inputText}
+                placeholder="Password..." 
+                placeholderTextColor="#003f5c"
+                onChangeText={text => setPassword(text)}/>
+            </View>
+            <TouchableOpacity style={styles.loginBtn} onPress={() => signInUser(email, password)}>
+              <Text style={styles.loginText}>LOGIN/SIGNUP</Text>
+            </TouchableOpacity>
+    
+      
+          </View>
+        );
     }
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#003f5c',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo:{
+    fontWeight:"bold",
+    fontSize:50,
+    color:"#fb5b5a",
+    marginBottom:40
+  },
+  inputView:{
+    width:"80%",
+    backgroundColor:"#465881",
+    borderRadius:25,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20
+  },
+  inputText:{
+    height:50,
+    color:"white"
+  },
+  forgot:{
+    color:"white",
+    fontSize:11
+  },
+  loginBtn:{
+    width:"80%",
+    backgroundColor:"#fb5b5a",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
+  },
+  loginText:{
+    color:"white"
+  }
+});
 
 export default App;
