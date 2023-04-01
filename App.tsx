416d3f760c32044c2fcaa85a07ca33a37a07cc05
jakilgo <justin.kilgo314@gmail.com>
2023-04-01 04:27:07 -0500

    adjust file layout for signin styles

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Home, Messages, Profile, Chat } from "./screens";
-import { PRIMARY_COLOR, DARK_GRAY, BLACK, WHITE } from "./assets/styles";
+import { PRIMARY_COLOR, DARK_GRAY, BLACK, WHITE, signupStyles } from "./assets/styles";
 import TabBarIcon from "./components/TabBarIcon";
 import "react-native-url-polyfill/auto"
 import { onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
@@ -141,25 +141,25 @@ const signInUser = (email: string, password: string) => {
         )
       } else {
         return (
-          <View style={styles.container}>
-            <Text style={styles.logo}>Treender</Text>
-            <View style={styles.inputView} >
+          <View style={signupStyles.container}>
+            <Text style={signupStyles.logo}>Treender</Text>
+            <View style={signupStyles.inputView} >
               <TextInput  
-                style={styles.inputText}
+                style={signupStyles.inputText}
                 placeholder="Email..." 
                 placeholderTextColor="#003f5c"
                 onChangeText={text => setEmail(text)}/>
             </View>
-            <View style={styles.inputView} >
+            <View style={signupStyles.inputView} >
               <TextInput  
                 secureTextEntry
-                style={styles.inputText}
+                style={signupStyles.inputText}
                 placeholder="Password..." 
                 placeholderTextColor="#003f5c"
                 onChangeText={text => setPassword(text)}/>
             </View>
-            <TouchableOpacity style={styles.loginBtn} onPress={() => signInUser(email, password)}>
-              <Text style={styles.loginText}>LOGIN/SIGNUP</Text>
+            <TouchableOpacity style={signupStyles.loginBtn} onPress={() => signInUser(email, password)}>
+              <Text style={signupStyles.loginText}>LOGIN/SIGNUP</Text>
             </TouchableOpacity>
     
       
@@ -167,46 +167,5 @@ const signInUser = (email: string, password: string) => {
         );
     }
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: WHITE,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo:{
-    fontWeight:"bold",
-    fontSize:50,
-    color:PRIMARY_COLOR,
-    marginBottom:40
-  },
-  inputView:{
-    width:"80%",
-    backgroundColor:"#CBC3E3",
-    borderRadius:25,
-    height:50,
-    marginBottom:20,
-    justifyContent:"center",
-    padding:20
-  },
-  inputText:{
-    height:50,
-    color:"white"
-  },
-  loginBtn:{
-    width:"80%",
-    backgroundColor:PRIMARY_COLOR,
-    borderRadius:25,
-    height:50,
-    alignItems:"center",
-    justifyContent:"center",
-    marginTop:40,
-    marginBottom:10
-  },
-  loginText:{
-    color:"white"
-  }
-});
 
 export default App;
