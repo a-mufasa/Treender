c74d4d22ee4eaf5e9a10091eefe7f675f65222f6
a-mufasa <ahmedr.moustafa@gmail.com>
2023-04-01 03:50:03 -0500

    update chat ui 2

--- screens/index.ts
+++ screens/index.ts
@@ -1,5 +1,4 @@
 export { default as Home } from "./Home";
-export { default as Matches } from "./Matches";
 export { default as Messages } from "./Messages";
 export { default as Profile } from "./Profile";
 export { default as Signup } from "./Signup";
